6000e9be9a07f91d6d3e4722ddd152466ba96fef
walt speelman <2963975+waltiam@users.noreply.github.com>
2017-06-12 15:46:47 -0400

    using @type/* for typings

--- problem_0001/src/Index.ts
+++ problem_0001/src/Index.ts
@@ -1,3 +1,5 @@
-export function isMultipleOf(value:number, factor:number):boolean{
-    return (value % factor ) === 0;
+export function isMultipleOf(value: number, factor: number) {
+    return function (): boolean {
+        return value % factor === 0;
+    };
 }
