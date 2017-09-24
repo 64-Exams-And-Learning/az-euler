d9d02230ca0e55640860a6bc296288400ba9c446
Walt <2963975+waltiam@users.noreply.github.com>
2017-09-24 14:25:00 -0400

    missed some cached changes

--- javascript/Euler_003/src/primes.js
+++ javascript/Euler_003/src/primes.js
@@ -1,15 +1,13 @@
-(function(){
-
+exports.isPrime = (x) =>{
     // a prime number test test for `x`
     // - if `x` mod `p` == 0 then not prime
-    var isPrime = function(testValue){
         var primes = [1],
             isPrime = true,
             i = primes.length;
         do {
 i =- 1;
 if (i ===0){isPrime = false; break;}
-            isPrime = testValue 
+            isPrime = x; 
         } while (isPrime);
-    }
-})();
+
+};
