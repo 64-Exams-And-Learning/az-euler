f515958ec453f67af07ac08b92cf5c8eb52a376e
walt <2963975+waltiam@users.noreply.github.com>
2017-09-24 17:05:01 -0400

    working on the prime nmber finder

--- javascript/Euler_003/src/primes.js
+++ javascript/Euler_003/src/primes.js
@@ -1,13 +1,26 @@
-exports.isPrime = (x) =>{
-    // a prime number test test for `x`
-    // - if `x` mod `p` == 0 then not prime
-        var primes = [1],
-            isPrime = true,
-            i = primes.length;
-        do {
-i =- 1;
-if (i ===0){isPrime = false; break;}
-            isPrime = x; 
-        } while (isPrime);
+var _ = require('lodash');
 
+exports.isPrime = function(x) {
+    // primes is the memoized list of primes
+    var primes = [1],
+        currentComposite = 1;
+
+    function isPrimeNumber(testValue) {
+        if (_.includes(primes, testValue)) return true;
+        if (currentComposite >= testValue) return false;
+        for (var prime of primes) {
+            if (isEvenlyDivisible(testValue, prime) && prime !== 1) return false;
+        }
+        return true;
+    }
+
+    if (isPrimeNumber(primes, currentComposite, x)) {
+        primes.push(x);
+        return true;
+    }
+    return false;
 };
+
+function isEvenlyDivisible(dividend, divisor) {
+    return (dividend % divisor === 0);
+}
--- javascript/Euler_003/test/primes.spec.js
+++ javascript/Euler_003/test/primes.spec.js
@@ -1,10 +1,18 @@
 var assert = require('assert'),
     should = require('chai').should(),
-    isPrime = require('../src/prime.js');
+    primes = require('../src/primes.js');
 
-describe('isPrime',function(){
-    it('should return true for 1',function(){
-        isPrime(1).should.equal(true);
-
+describe('isPrime', function() {
+    it('should return true for 1', function() {
+        primes.isPrime(1).should.equal(true);
+    });
+    it('should return true for 2', function() {
+        primes.isPrime(2).should.equal(true);
+    });
+    it('should return true for 3', function() {
+        primes.isPrime(3).should.equal(true);
+    });
+    it('should return false for 4', function() {
+        primes.isPrime(4).should.equal(false);
     });
 });
