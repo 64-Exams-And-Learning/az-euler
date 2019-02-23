00117ebb0a7841361e7747bd7515ce0d5fc44777
walt <2963975+waltiam@users.noreply.github.com>
2019-02-23 11:44:46 -0500

    prime finder in typescript
    still need to add some optimizations

--- typescript/src/Primes.ts
+++ typescript/src/Primes.ts
@@ -1,5 +1,54 @@
-export function isPrime(value:number):boolean{
+export class Primes {
+    static Known: number[] = [];
+    static TestedCeiling: number = 1;
 
-    // first, it must be an integer
-    return value % 1===0;
+    static isEvenlyDivisible = (dividend: number, divisor: number): boolean => dividend % divisor === 0;
+
+    static isNewPrime = (value: number): boolean => {
+        // console.log(`Known: ${Primes.Known}`);
+        // console.log(`Value: ${value}`);
+        var maxFloor = value;
+        var isComposite = false;
+
+        for (var e = 0; e < Primes.Known.length; e++) {
+            if (Primes.isEvenlyDivisible(value, Primes.Known[e])) {
+                isComposite = true;
+                break; // found a divisor, don't continue
+            }
+        }
+
+        // the value was not flagged as a composite
+        if (!isComposite) {
+            Primes.Known.push(value);
+        }
+
+        // in any case set the new upper limit 
+        Primes.TestedCeiling = value;
+        return !isComposite; // which will be false if it is prime
+    }
+}
+
+export function Range(size: number, start = 0): number[] {
+    return [...Array(size).keys()].map(i => i + start);
+}
+
+export function isPrime(value: number): boolean {
+    // Prime numbers are the natural numbers greater than one that are not products of two smaller numbers
+    // This allows us a couple of quick tests to get started
+    if (value % 1 !== 0) return false; // must be a natural (integer) number
+    if (value <= 1) return false; // must be greater then 1
+
+    // It may be in our list of known primes
+    if (Primes.Known.includes(value)) return true;
+    // We many have tested a lesser number, and since it wasn't prime it must be composite
+    if (value <= Primes.TestedCeiling) return false;
+
+    var finalValue = true;
+
+    // loop from TestedCeiling to passed in value
+    for (var i = Primes.TestedCeiling + 1; i <= value; i++) {
+        finalValue = Primes.isNewPrime(i);
+    }
+
+    return finalValue;
 }
--- typescript/test/Primes.spec.ts
+++ typescript/test/Primes.spec.ts
@@ -3,13 +3,22 @@ const should: Chai.Should = chai.should();
 
 import "mocha";
 
-import * as f from "../src/Primes";
+import f = require('../src/Primes')
 
-describe("Test if a value is prime",()=>{
-    it("1 should be prime",()=>f.isPrime(1).should.equal(true));
-    it("1.2 should not be prime",()=> f.isPrime(1.2).should.equal(false));
-    it("2 should be prime",()=>f.isPrime(2).should.equal(true));
-    it("2.2 should not be prime",()=>f.isPrime(2.2).should.equal(false));
-    it("3 should be prime",()=>f.isPrime(3).should.equal(true));
-    it("4 should not be prime",()=>f.isPrime(4).should.equal(false));
+describe("Test if a value is prime", () => {
+    it("1 should not be prime", () => f.isPrime(1).should.equal(false));
+    it("1.2 should not be prime", () => f.isPrime(1.2).should.equal(false));
+    it("2 should be prime", () => f.isPrime(2).should.equal(true));
+    it("2.2 should not be prime", () => f.isPrime(2.2).should.equal(false));
+    it("3 should be prime", () => f.isPrime(3).should.equal(true));
+    it("4 should not be prime", () => f.isPrime(4).should.equal(false));
+    it("10 should not be prime", () => f.isPrime(10).should.equal(false));
+    it("11 should be prime", () => f.isPrime(11).should.equal(true));
+});
+
+describe("Test the Range utility", () => {
+    it("Range(3) should be [0,1,2]", () => f.Range(3).should.deep.equal([0, 1, 2]));
+    it("Range(3,1) should be [1,2,3]", () => f.Range(3, 1).should.deep.equal([1, 2, 3]));
+    it("Range(3,-1) should be [-1,0,1]", () => f.Range(3, -1).should.deep.equal([-1, 0, 1]));
+    it("Range(3,10) should be [10,11,12]", () => f.Range(3, 10).should.deep.equal([10, 11, 12]));
 });
